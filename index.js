a21522e9059e5bb2ac96963193af21521bfb1e74
hossein-cholaki <110971150+hossein-cholaki@users.noreply.github.com>
2023-01-16 23:58:23 +0330

    paractice more

--- index.js
+++ index.js
@@ -42,8 +42,40 @@
 
 
 // Math.max 
-const numbers = [12,4,145,478,495,145,0,100]
-console.log(numbers)
+// const numbers = [12,4,145,478,495,145,0,100]
+// console.log(numbers)
 
-console.log(...numbers)
-console.log(Math.max(...numbers));
+// console.log(...numbers)
+// console.log(Math.max(...numbers));
+
+// const data ={
+//     temp1:'001',
+//     temp2:'002',
+//     fName:'hossein',
+//     lName:'cholaki'
+// }
+// const{temp1, ...restOfData}=data
+// console.log(temp1)
+// console.log({...restOfData})
+// console.log(...restOfData)
+
+const circleX ={
+    name:"circleX",
+    radius:5,
+   }
+   const circleY ={
+    name:"circleY",
+    
+   }
+  // PI * r *r
+   const circleArea = ({radius=6})=>{
+  const area =(Math.PI * radius * radius).toFixed(2);
+  const area1 =(Math.PI * radius * radius);
+  console.log(Math.PI);
+  console.log(radius)
+  console.log(area) ;
+  console.log(area1) ;
+  console.log('--------------------------------------') ;
+   }
+   circleArea(circleX);
+   circleArea(circleY)
