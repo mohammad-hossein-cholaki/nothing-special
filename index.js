8c8616fe1c091cc481228930518bc5bee9a82706
hossein-cholaki <110971150+hossein-cholaki@users.noreply.github.com>
2023-01-16 22:24:10 +0330

    d-3

---
+++ index.js
@@ -0,0 +1,35 @@
+// const data ={
+//   temp1:"001",
+//   temp2:"002",
+//   firstName:"joodi",
+//   lastName:"aboat",
+// }
+
+// const {temp1,temp2,...restOfData} = data;
+// console.log(temp1);
+// console.log(temp2);
+// console.log(restOfData);
+
+// const newArr = [{...restOfData}];
+// const newObj ={...restOfData}
+// console.log(newArr);
+// console.log(newObj);
+// function showlaptop ({ram,hard,cpu}){
+//   console.log(`Your Laptop Has ram of ${ram} 
+//   and hard of ${hard} and cpu With ${cpu}`)
+// }
+
+// const laptop1 = {
+//   ram:32,
+//   hard:250,
+//   cpu:"corei7"
+
+// }
+// const laptop2 = {
+//   ram:50,
+//   hard:250,
+//   cpu:"corei5"
+
+// }
+// const {ram,hard,cpu} =laptop1;
+//  showlaptop(laptop2);
